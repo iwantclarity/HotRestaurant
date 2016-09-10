5b2f24e4d68c33e1cc3ca1f5a70c1b9a3daca412
David Forman <djforman@gmail.com>
2016-09-10 13:37:22 -0400

    Added back a folder

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -5,15 +5,15 @@ var app = express();
 // Basic route that sends the user to the index AJAX Page
 app.get('/', function(req, res){
 
-	res.sendFile(path.join(__dirname + 'app/public/index.html'));
+	res.sendFile(path.join(__dirname + './public/index.html'));
 })
 // Basic route that sends the user to the make AJAX Page
 app.get('/', function(req, res){
 
-	res.sendFile(path.join(__dirname + 'app/public/make.html'));
+	res.sendFile(path.join(__dirname + './public/make.html'));
 })
 // Basic route that sends the user to the view AJAX Page
 app.get('/', function(req, res){
 
-	res.sendFile(path.join(__dirname + 'app/public/view.html'));
+	res.sendFile(path.join(__dirname + './public/view.html'));
 })
