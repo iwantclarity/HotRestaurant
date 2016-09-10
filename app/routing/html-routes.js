68152e2b2191156603fe06a851946d2046b9d82b
David Forman <djforman@gmail.com>
2016-09-10 13:33:25 -0400

    Added new route url

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -5,15 +5,15 @@ var app = express();
 // Basic route that sends the user to the index AJAX Page
 app.get('/', function(req, res){
 
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + 'app/public/index.html'));
 })
 // Basic route that sends the user to the make AJAX Page
 app.get('/', function(req, res){
 
-	res.sendFile(path.join(__dirname + '/make.html'));
+	res.sendFile(path.join(__dirname + 'app/public/make.html'));
 })
 // Basic route that sends the user to the view AJAX Page
 app.get('/', function(req, res){
 
-	res.sendFile(path.join(__dirname + '/view.html'));
+	res.sendFile(path.join(__dirname + 'app/public/view.html'));
 })
